416676da1ddb137309b050618213420a90effa18
Chris Sevilleja <chris@scotch.io>
2017-02-26 21:07:10 -0800

    final

--- js/magic.js
+++ js/magic.js
@@ -5,8 +5,15 @@ const person = {
 
 console.log(person.name);
 
-let myVar         = `blah ${person.name}`;
+let myVar           = `blah ${person.name}`;
 const myNumber      = 3;
 const myStringggggg =  'here';
+
+function doSomething() {
+  return myVar;
+}
+
+doSomething();
+console.log(myVar);
 
 myVar = 'something else';
